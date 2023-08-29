ff6cafcffeb6e49d0959d33fce6564069f99510c
Wandati <Petersimon801@gmail.com>
2023-08-29 20:39:10 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ("Mr. Chocolate", 20,"M","melancholic",1);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ("Rowdy", 10,"M","melancholic",0);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ("Tabitha", 15,"F","sanguine",1);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ("Sergeant Brown", 30,"M","melancholic",1);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ("Melissa", 25,"F","phlegmatic",1);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ("Grinch", 10,"M","melancholic",0);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES ("Wendy", 18,"M","chloleric",1);
+INSERT INTO bears (name, age, sex, temperament, alive) VALUES (NULL, 24,"M","sanguine",1);
